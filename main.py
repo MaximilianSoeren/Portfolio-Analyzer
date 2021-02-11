960a76d0d53efd0728bbe652dfc1687f1ce31e0a
MaximilianSoeren <74557518+MaximilianSoeren@users.noreply.github.com>
2021-02-11 12:39:55 +0100

    awd

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ imports.st.markdown(darkmode, unsafe_allow_html=True)
 expander = imports.st.beta_expander(label="Upload")
 with expander:
     col1, col2, col3 = imports.st.beta_columns(3)
-    if col1.imports.st.button("IGN", key=1):
+    if imports.col1.st.button("IGN", key=1):
         uploaded_file = imports.st.file_uploader("", type=".csv", key=1)
         try:
             fc.reading_csv_ing(uploaded_file)
         except:
             bank = 'Consors'
             print("Bitte lade eine CSV Datei von ING hoch.")
-    if col2.imports.st.button("CONSORS", key=2):
+    if imports.col2.st.button("CONSORS", key=2):
         uploaded_file = imports.st.file_uploader("", type=".csv", key=2)
         try:
             fc.reading_csv_consors(uploaded_file)
         except:
             bank = 'Consors'
             print("Bitte lade eine CSV Datei von Consors hoch.")
-    if col3.imports.st.button("COMDIRECT", key=3):
+    if imports.col3.st.button("COMDIRECT", key=3):
         uploaded_file = imports.st.file_uploader("", type=".csv", key=3)
         try:
             fc.reading_csv_comdirect(uploaded_file)
